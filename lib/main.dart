fba2979ee6840dcd2ce395c471d678894f4f0453
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 07:54:06 +0700

    Menambahkan implementasi BlocProvider dan StateProvider untuk mengelola state nama dan email pada aplikasi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_application_1/bloc/blocku.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:flutter_application_1/setstate/setstate.dart';
+
+final namaProvider = StateProvider<String>((ref) => "");
+final emailProvider = StateProvider<String>((ref) => "");
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    BlocProvider(
+      create: (context) => FormBloc(),
+      child: MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple)
       ),
+      home: FormPage4()
     );
   }
 }
