1ed43507f07483c3a697b521bfcb28d1f5f51a91
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 07:39:26 +0700

    Menambahkan file provider.dart dengan implementasi ChangeNotifierProvider untuk mengelola state aplikasi.

---
+++ lib/provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => FormProvider(),
+      child: MyApp(),
+    ),
+  );
+}
