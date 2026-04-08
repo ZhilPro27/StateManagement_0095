4cc37de7406450a2bb7064955d5faafc3ad45600
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 07:28:35 +0700

    Menambahkan UI untuk FormBloc dengan input nama dan email serta menampilkan pesan sukses saat submit.

--- lib/bloc/blocku.dart
+++ lib/bloc/blocku.dart
@@ -33,3 +33,46 @@ class FormBloc extends Bloc<FormEvent, FormState> {
     });
   }
 }
+
+//UI
+class FormPage4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Bloc")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocBuilder<FormBloc, FormState>(
+          builder: (context, state) {
+            return Column(
+              children: [
+                TextField(
+                  onChanged: (value) => context.read<FormBloc>().add(NamaChanged(value)),
+                  decoration: InputDecoration(labelText: "Nama"),
+                ),
+                TextField(
+                  onChanged: (value) => context.read<FormBloc>().add(EmailChanged(value)),
+                  decoration: InputDecoration(labelText: "Email"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(//content: Text("Nama: ${state.nama}, Email: ${state.email}"
+                      content: Text("Sumbit Sukses!"),
+                      )
+                    );
+                  }, 
+                  child: Text("Submit"),
+                ),
+                SizedBox(height: 10),
+
+                Text("Nama: ${state.nama}"),
+                Text("Email: ${state.email}"),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
